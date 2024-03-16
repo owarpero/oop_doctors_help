6edf68ca0532341efb6c71f053882a018976fa27
AlexYrkn <61319606+AlexYrkn@users.noreply.github.com>
2024-03-16 17:06:03 +0300

    repos interfaces

---
+++ src/Infrastructure/DoctorsHelp.Infrastructure.Persistence/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using DoctorsHelp.Application.Models;
+using Infrastructure.Persistence.Models;
+
+namespace DoctorsHelp.Infrastructure.Persistence.Interfaces;
+
+public interface IEmployeeRepository
+{
+    void Add(Employee employee);
+
+    EmployeeModel GetEmployee(int id);
+
+    void UpdateEmployee(Employee employee);
+
+    bool Delete(Employee employee);
+}
---
+++ src/Infrastructure/DoctorsHelp.Infrastructure.Persistence/Interfaces/IScheduleRepository.cs
@@ -0,0 +1,15 @@
+﻿using DoctorsHelp.Application.Models;
+using Infrastructure.Persistence.Models;
+
+namespace DoctorsHelp.Infrastructure.Persistence.Interfaces;
+
+public interface IScheduleRepository
+{
+    void Add(Schedule schedule);
+
+    ScheduleModel GetSchedule(int id);
+
+    void Update(Schedule schedule);
+
+    bool Delete(Schedule schedule);
+}
